b542659d3f01c9ae3d03862c611b5864512b14c8
Team-Alphas <85256177+Team-Alphas@users.noreply.github.com>
2021-07-23 00:50:10 +0530

    Update __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -69,7 +69,7 @@ For all command use / [or](https://telegra.ph/file/b1b545eb4877b012eac82.png) !
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\nKindly use ! for commands if / is not working\n")
 
-TOKISAKI_IMG = "https://telegra.ph/file/5a5a7a30253abc69ff154.jpg"
+TOKISAKI_IMG = "https://telegra.ph/file/6d8a594fc339f69cf0d6b.jpg"
 KURUMI_IMG = "https://telegra.ph/file/e1d87ec2bdac4e3cfe0a4.mp4"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
